2e776cf188ccf3e106e8f7ad68be2da707301e54
André Ezequiel <andrelucasmartins@hotmail.com>
2023-12-19 00:10:13 -0300

    chore: add new params dynamic force-dynamic

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -1,19 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
-import Prose from "@/components/prose";
-import { getPage } from "@/lib/shopify";
-import { notFound } from "next/navigation";
+import Prose from '@/components/prose'
+import { getPage } from '@/lib/shopify'
+import { notFound } from 'next/navigation'
 
-export const revalidate = 43200; // 12 hours in seconds
+export const dynamic = 'force-dynamic'
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { page: string };
-}): Promise<Metadata> {
-  const page = await getPage(params.page);
+export const revalidate = 43200 // 12 hours in seconds
 
-  if (!page) return notFound();
+export async function generateMetadata({ params }: { params: { page: string } }): Promise<Metadata> {
+  const page = await getPage(params.page)
+
+  if (!page) return notFound()
 
   return {
     title: page.seo?.title || page.title,
@@ -21,30 +19,27 @@ export async function generateMetadata({
     openGraph: {
       publishedTime: page.createdAt,
       modifiedTime: page.updatedAt,
-      type: "article",
+      type: 'article',
     },
-  };
+  }
 }
 
 export default async function Page({ params }: { params: { page: string } }) {
-  const page = await getPage(params.page);
+  const page = await getPage(params.page)
 
-  if (!page) return notFound();
+  if (!page) return notFound()
 
   return (
     <>
       <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
       <Prose className="mb-8" html={page.body as string} />
       <p className="text-sm italic">
-        {`This document was last updated on ${new Intl.DateTimeFormat(
-          undefined,
-          {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          },
-        ).format(new Date(page.updatedAt))}.`}
+        {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        }).format(new Date(page.updatedAt))}.`}
       </p>
     </>
-  );
+  )
 }
