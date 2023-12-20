4f6be16ea6fa1df56f82cc588a59e36443b2d8c4
André Ezequiel <andrelucasmartins@hotmail.com>
2023-12-20 20:16:50 -0300

    chore: deploy static

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
   // distDir: "dist",
   reactStrictMode: true,
   images: {
-    // unoptimized: true,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
